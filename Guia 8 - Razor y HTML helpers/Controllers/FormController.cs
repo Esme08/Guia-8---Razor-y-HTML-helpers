b2e4ca788d615efe79fa0e801f2a2cb5fb23570a
esme <esmeralda.cardona1@catolica.edu.sv>
2024-04-05 21:59:26 -0600

    Agregar archivos de proyecto.

---
+++ Guia 8 - Razor y HTML helpers/Controllers/FormController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Guia_8___Razor_y_HTML_helpers.Controllers
+{
+    public class FormController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Guia 8 - Razor y HTML helpers/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Guia_8___Razor_y_HTML_helpers.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
